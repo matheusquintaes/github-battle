2e1e001475430aeebdff98a3572c6cc9bcae3abf
Matheus Quintaes <matheus.quintaes@gmail.com>
2019-11-02 18:52:24 +1300

    Fetch Repos

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { fetchPopularRepos } from '../utils/api'
 
-function LangaugesNav ({ selected, onUpdateLanguage }) {
+function LanguagesNav ({ selected, onUpdateLanguage }) {
   const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
 
   return (
@@ -20,7 +21,7 @@ function LangaugesNav ({ selected, onUpdateLanguage }) {
   )
 }
 
-LangaugesNav.propTypes = {
+LanguagesNav.propTypes = {
   selected: PropTypes.string.isRequired,
   onUpdateLanguage: PropTypes.func.isRequired
 }
@@ -30,27 +31,56 @@ export default class Popular extends React.Component {
     super(props)
 
     this.state = {
-      selectedLanguage: 'All'
+      selectedLanguage: 'All',
+      repos: null,
+      error: null,
     }
 
     this.updateLanguage = this.updateLanguage.bind(this)
+    this.isLoading = this.isLoading.bind(this)
+  }
+  componentDidMount () {
+    this.updateLanguage(this.state.selectedLanguage)
   }
   updateLanguage (selectedLanguage) {
     this.setState({
-      selectedLanguage
+      selectedLanguage,
+      error: null,
+      repos: null
     })
+
+    fetchPopularRepos(selectedLanguage)
+      .then((repos) => this.setState({
+        repos,
+        error: null,
+      }))
+      .catch(() => {
+        console.warn('Error fetching repos: ', error)
+
+        this.setState({
+          error: `There was an error fetching the repositories.`
+        })
+      })
+  }
+  isLoading() {
+    return this.state.repos === null && this.state.error === null
   }
   render() {
-    const { selectedLanguage } = this.state
+    const { selectedLanguage, repos, error } = this.state
 
     return (
       <React.Fragment>
-        <LangaugesNav
+        <LanguagesNav
           selected={selectedLanguage}
           onUpdateLanguage={this.updateLanguage}
         />
+
+        {this.isLoading() && <p>LOADING</p>}
+
+        {error && <p>{error}</p>}
+
+        {repos && <pre>{JSON.stringify(repos, null, 2)}</pre>}
       </React.Fragment>
     )
   }
-} 
-
+}
