dfd683f9cc0a971d5b14461339fb03c1ea74e5b8
Matheus Quintaes <matheus.quintaes@gmail.com>
2019-11-07 19:24:39 +1300

    Theme Provider

--- app/index.js
+++ app/index.js
@@ -1,17 +1,35 @@
 import React from 'react'
-import ReactDOM from  'react-dom'
+import ReactDOM from 'react-dom'
 import './index.css'
 import Popular from './components/Popular'
 import Battle from './components/Battle'
+import { ThemeProvider } from './contexts/theme'
 
 class App extends React.Component {
-   render() {
-     return (
-      <div>  <Battle /></div>
-     )
-   }
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      theme: 'light',
+      toggleTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: theme === 'light' ? 'dark' : 'light'
+        }))
+      }
+    }
+  }
+  render() {
+    return (
+      <ThemeProvider value={this.state}>
+        <div className='container'>
+          <Battle />
+        </div>
+      </ThemeProvider>
+    )
+  }
 }
 
 ReactDOM.render(
-  <App/>, document.getElementById('app')
+  <App />,
+  document.getElementById('app')
 )
